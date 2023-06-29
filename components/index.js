df320e2f6126fe17b2ca252039ef73a3a897699d
Phạm Nguyễn Hùng Anh <hunganh130502@gmail.com>
2023-06-29 20:14:29 +0700

    UI login & Recipe Srceen

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import TabIcon from "./TabIcon";
+import CategoryCard from "./CategoryCard";
+import TrendingCard from "./TrendingCard";
+import Viewers from "./Viewers";
+
+export { TabIcon, CategoryCard, TrendingCard, Viewers };
